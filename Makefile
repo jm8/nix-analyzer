4c6ed134d2dc3c73a1e103dfa1b8ffba44cd0a8f
Josh <jm8hjkl@gmail.com>
2022-12-02 14:58:22 -0500

    think about life

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS:=-O3 -Wall $(shell pkg-config --cflags --libs nix-main bdw-gc nlohmann_json) -I$(boostInclude) -DNIX_VERSION=\"$(NIX_VERSION)\" -lnixutil -lnixstore -lnixexpr -lnixmain -lnixcmd -lgc
+CFLAGS:=-O3 -Wall -std=c++20 $(shell pkg-config --cflags --libs nix-main bdw-gc nlohmann_json) -I$(boostInclude) -DNIX_VERSION=\"$(NIX_VERSION)\" -lnixutil -lnixstore -lnixexpr -lnixmain -lnixcmd -lgc
 
 nix-analyzer: src/nix-analyzer.cpp
 	g++ $(CFLAGS) src/nix-analyzer.cpp -o nix-analyzer
--- src/nix-analyzer.cpp
+++ src/nix-analyzer.cpp
@@ -10,6 +10,7 @@
 #include "get-drvs.hh"
 #include "globals.hh"
 #include "local-fs-store.hh"
+#include "nixexpr.hh"
 #include "shared.hh"
 
 #include "store-api.hh"
@@ -42,6 +43,8 @@ struct NixAnalyzer
     void evalString(string s, Value &v);
 
     void printValue(std::ostream &s, Value &v);
+
+    void debugExpr(std::ostream &s, Expr *e, int indent = 0);
 };
 
 NixAnalyzer::NixAnalyzer(const Strings &searchPath, nix::ref<Store> store)
@@ -61,14 +64,44 @@ void NixAnalyzer::printValue(std::ostream &s, Value &v) {
     v.print(state->symbols, s);
 }
 
+bool poscmp(Pos a, Pos b) {
+    if (a.line > b.line)
+        return 1;
+    if (a.line < b.line)
+        return -1;
+    if (a.column > b.column)
+        return 1;
+    if (a.column < b.column)
+        return -1;
+    return 0;
+}
+
+// wip
+void evaluateSubexpression(EvalState &state, Env &env, Expr *expr,
+                           Pos targetPos, Value &v) {
+    if (auto *e = dynamic_cast<ExprAttrs *>(expr)) {
+        // find expr with last position before target
+        Expr *sub = nullptr;
+        for (auto &[symbol, attrdef] : e->attrs) {
+            if (!sub || poscmp(state.positions[attrdef.pos],
+                               state.positions[sub->getPos()]) > 0) {
+                sub = attrdef.e;
+            }
+        }
+        return evaluateSubexpression(state, env, sub, targetPos, v);
+    }
+    return state.eval(expr, v);
+}
+
 int main() {
     initNix();
     initGC();
 
     Strings searchPath;
     auto analyzer = make_unique<NixAnalyzer>(searchPath, openStore());
-    Value v;
-    analyzer->evalString("2 + 2", v);
-    analyzer->printValue(cout, v);
-    cout << endl;
+    Expr *root = analyzer->parseString("{a = 123; b = 456;}");
+    ExprAttrs *e = dynamic_cast<ExprAttrs *>(root);
+    auto [symbol, attrdef] = *e->attrs.begin();
+    auto pos = analyzer->state->positions[attrdef.pos];
+    cout << pos << endl;
 }
