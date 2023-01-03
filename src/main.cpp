41448bae4139d3d820bb379db76a4200c3e3e870
Josh <jm8hjkl@gmail.com>
2023-01-02 23:31:50 -0500

    textDocumentSync activated

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 #include <memory>
 #include <network/uri.hpp>
 #include <ostream>
+#include <string>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
 #include "LibLsp/JsonRpc/Condition.h"
@@ -19,18 +21,27 @@
 #include "LibLsp/lsp/ProtocolJsonHandler.h"
 #include "LibLsp/lsp/general/exit.h"
 #include "LibLsp/lsp/general/initialize.h"
+#include "LibLsp/lsp/general/initialized.h"
 #include "LibLsp/lsp/general/lsServerCapabilities.h"
 #include "LibLsp/lsp/general/lsTextDocumentClientCapabilities.h"
 #include "LibLsp/lsp/general/shutdown.h"
+#include "LibLsp/lsp/lsAny.h"
+#include "LibLsp/lsp/lsDocumentUri.h"
 #include "LibLsp/lsp/lsMarkedString.h"
 #include "LibLsp/lsp/lsp_completion.h"
 #include "LibLsp/lsp/textDocument/declaration_definition.h"
+#include "LibLsp/lsp/textDocument/did_change.h"
+#include "LibLsp/lsp/textDocument/did_close.h"
+#include "LibLsp/lsp/textDocument/did_open.h"
 #include "LibLsp/lsp/textDocument/document_symbol.h"
 #include "LibLsp/lsp/textDocument/hover.h"
 #include "LibLsp/lsp/textDocument/resolveCompletionItem.h"
 #include "LibLsp/lsp/textDocument/signature_help.h"
 #include "LibLsp/lsp/textDocument/typeHierarchy.h"
 #include "LibLsp/lsp/workspace/execute_command.h"
+
+#include "nix-analyzer.h"
+#include "store-api.hh"
 
 using namespace std;
 
@@ -64,6 +75,9 @@ class NixLanguageServer {
    public:
     RemoteEndPoint remoteEndPoint;
     DummyLog log{"log.txt"};
+    unique_ptr<NixAnalyzer> analyzer;
+    // map from url to document content
+    unordered_map<string, string> documents;
 
     struct OStream : lsp::base_ostream<std::ostream> {
         explicit OStream(std::ostream& _t) : base_ostream<std::ostream>(_t) {
@@ -82,10 +96,11 @@ class NixLanguageServer {
         }
     };
 
-    NixLanguageServer()
+    NixLanguageServer(nix::Strings searchPath, nix::ref<nix::Store> store)
         : remoteEndPoint(make_shared<lsp::ProtocolJsonHandler>(),
                          make_shared<GenericEndpoint>(log),
-                         log) {
+                         log),
+          analyzer(make_unique<NixAnalyzer>(searchPath, store)) {
         remoteEndPoint.registerHandler([&](const td_initialize::request& req) {
             td_initialize::response res;
             res.id = req.id;
@@ -95,14 +110,42 @@ class NixLanguageServer {
                 .resolveProvider = false,
                 .triggerCharacters = {{".", "${"}},
             }};
+            res.result.capabilities.textDocumentSync = {
+                {lsTextDocumentSyncKind::Full}, {}};
             return res;
         });
+
+        remoteEndPoint.registerHandler(
+            [&](Notify_InitializedNotification::notify& notify) {
+                log.info("initialized");
+            });
 
         remoteEndPoint.registerHandler([&](Notify_Exit::notify& notify) {
             log.info("exit: " + notify.jsonrpc);
             remoteEndPoint.stop();
             esc_event.notify(make_unique<bool>(true));
         });
+
+        remoteEndPoint.registerHandler(
+            [&](Notify_TextDocumentDidOpen::notify& notify) {
+                string uri = notify.params.textDocument.uri.raw_uri_;
+                log.info("didOpen: " + uri);
+                documents[uri] = notify.params.textDocument.text;
+            });
+
+        remoteEndPoint.registerHandler(
+            [&](Notify_TextDocumentDidChange::notify& notify) {
+                string uri = notify.params.textDocument.uri.raw_uri_;
+                log.info("didChange: " + uri);
+                documents[uri] = notify.params.contentChanges[0].text;
+            });
+
+        remoteEndPoint.registerHandler(
+            [&](Notify_TextDocumentDidClose::notify& notify) {
+                string uri = notify.params.textDocument.uri.raw_uri_;
+                log.info("didClose: " + uri);
+                documents.erase(uri);
+            });
 
         remoteEndPoint.registerHandler([&](const td_hover::request& req) {
             log.info("hover");
@@ -112,9 +155,16 @@ class NixLanguageServer {
         });
 
         remoteEndPoint.registerHandler([&](const td_completion::request& req) {
+            log.info("completion");
             td_completion::response res;
+            string url = req.params.textDocument.uri.raw_uri_;
+            auto it = documents.find(url);
+            if (it == documents.end()) {
+                return res;
+            }
+            string_view content{it->second};
             res.result.items.push_back(lsCompletionItem{
-                .label = "aaa",
+                .label = "aaa" + string(1, content[0]),
                 .kind = {lsCompletionItemKind::Function},
                 .detail = {"Scream"},
             });
@@ -138,6 +188,10 @@ class NixLanguageServer {
 };
 
 int main() {
-    NixLanguageServer server;
+    nix::initNix();
+    nix::initGC();
+    nix::Strings searchPath;
+    NixLanguageServer server(searchPath, nix::openStore());
+
     server.esc_event.wait();
 }
