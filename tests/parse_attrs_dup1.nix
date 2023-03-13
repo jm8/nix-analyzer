8e3085021e9a0d1a1ca2027bad080e6f2d66f342
josh <jm8hjkl@gmail.com>
2023-03-13 18:32:01 -0400

    add duplicate attr tests

---
+++ tests/parse_attrs_dup1.nix
@@ -0,0 +1,11 @@
+import ./lib/maketest.nix {
+  type = "parse";
+  source = ''
+    { x = 123; y = 456; x = 789; }
+  '';
+  expected = ''
+    { x = 123; y = 456; }
+  '';
+  expectedErrors = ["duplicate attr 0:20-0:28"];
+  expectedExprPath = ["ExprAttrs"];
+}
---
+++ tests/parse_attrs_dup2.nix
@@ -0,0 +1,13 @@
+import ./lib/maketest.nix {
+  type = "parse";
+  source = ''
+    { services.ssh.port = 22; services.ssh^.port = 23; }
+  '';
+  expected = ''
+    { services = { ssh = { port = 22; }; }; }
+  '';
+  expectedAttrPath = "services.ssh.port";
+  expectedAttrPathIndex = 1;
+  expectedErrors = ["duplicate attr 0:26-0:49"];
+  expectedExprPath = ["ExprAttrs"];
+}
