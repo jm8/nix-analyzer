98bacab3e85151b4045f0aa8105648c7d30fc2d0
josh <jm8hjkl@gmail.com>
2023-03-15 12:07:42 -0400

    parse let missing semicolon

---
+++ tests/parse_let_typing_missing_semicolon.nix
@@ -0,0 +1,11 @@
+import ./lib/maketest.nix {
+  type = "parse";
+  source = ''
+    let a = 4 in b^
+  '';
+  expected = ''
+    (let a = 4; in b)
+  '';
+  expectedErrors = ["expected ';', got 'IN' 0:10-0:13"];
+  expectedExprPath = ["ExprVar" "ExprLet"];
+}
