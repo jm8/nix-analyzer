443f81f39656efb9df9b46326936cc1177a3d9a4
josh <jm8hjkl@gmail.com>
2023-04-29 17:52:14 -0400

    add an extra slash

--- src/common/loadfile.cpp
+++ src/common/loadfile.cpp
@@ -4,7 +4,7 @@
 nix::Value* loadFile(nix::EvalState& state, std::string path) {
     auto v = state.allocValue();
     try {
-        state.evalFile(RESOURCEPATH + path, *v);
+        state.evalFile(std::string{RESOURCEPATH} + "/" + path, *v);
     } catch (nix::Error& e) {
         REPORT_ERROR(e);
         v->mkNull();
