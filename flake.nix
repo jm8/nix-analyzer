0a71378e924e47a886efdad324cd50dc0273657e
Joshua Sims <jm8hjkl@gmail.com>
2024-11-23 14:22:15 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "nix-analyzer";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.nix-eval-server.url = "path:/var/home/josh/src/nix-analyzer-new/nix-eval-server";
+
+  outputs = { self, flake-utils, rust-overlay, nix-eval-server, nixpkgs }: 
+    flake-utils.lib.eachDefaultSystem (system: 
+    let 
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rust-bin.nightly.latest.default  
+          nix-eval-server.packages.${system}.nix-eval-server
+        ];
+      };
+    });
+}
+
